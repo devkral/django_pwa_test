728625e3dda586d80abeb5b7a658d893cf7cfb7b
alex <devkral@web.de>
2018-02-05 10:25:59 +0100

    bugfixes, simple django 1.11 compatibility

--- pwa_1/urls.py
+++ pwa_1/urls.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+try:
+    from django.urls import path, include
+except ImportError:
+    from django.conf.urls import include
+    from django.conf.urls import url as path
 
 from django.views.generic.base import TemplateView
 
